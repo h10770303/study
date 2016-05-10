2be2fe4040a464cac9f7ead220525e060f0593eb
hu_mingwei <hu_mingwei@cdv.com>
2016-05-10 17:22:54 +0800

    py代码提交

--- varType.py
+++ varType.py
@@ -1,13 +1,65 @@
 #coding=utf-8
 
+# 8. 数据类型的转换
+# ints = 6
+# print  ints
+# # print str(ints)
+# print long(ints)
+# print complex(ints)
+
+# name="humingwei"
+# ints=ord(name)
+# print ints
+
 # 3 标准数据类型
-intVar=100;
-floatVar=100.0
-#del intVar,floatVar  #删除变量
-print intVar;
+# intVar=100;
+# floatVar=100.0
+# #del intVar,floatVar  #删除变量
+# print intVar;
 
+# 4.字符串类型
+# name="hello world 胡明伟";
+# name2=" welcome study python "
+# print "打印name:"+name;
+# print "打印第一个字符："+name[0]
+# print "打印1-4个字符："+name[0:4]
+# print "从第9个字符往后："+name[9:]
+# print "倒叙1到3"+name[-1:-3]
+# print "倒叙1到正序1："+name[:-1]
+# print  name+name2
+# print name *2
 
+#5. []列表的使用
+# ints=[1,2,3,4,5,6];
+# names=['胡明伟','张三','李四','王五'];
+# # print "输出所有字符"+ints   # 该是报错的， 字符串不能喝列表共同打印
+# print ints #打印所有
+# print ints[0]
+# print ints[2:5]
+# print names
+# print names[3:5]
+# print ints+names
+# print names*2
 
+# 6. 元祖（）用()表示的元祖和列表比较类似， 不同的是元祖是只读的，内容不能修改
+# ints=(1,2,3,4,5,6)
+# names=('胡明伟','humingwei','张三')
+# print ints
+# print names
+# print ints[0:5]
+# ints[1]=10;print ints  # 不能修改 只读
+
+# 7. 字典： key value {}
+# dic={}
+# dic['one']='胡明伟';
+# dic['1']='humingwei'
+# json={'name':'胡明伟','age':27,'height':172}
+# print dic
+# print json
+# print json['name']
+# print json.keys();
+# print json.values()
+# print json.clear()
 
 
 
