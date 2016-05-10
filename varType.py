1ce8737292cf23a13cd22d8fc6a64da77dfa3c7e
hu_mingwei <hu_mingwei@cdv.com>
2016-05-10 11:11:52 +0800

    py代码提交

---
+++ varType.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+# 3 标准数据类型
+intVar=100;
+floatVar=100.0
+#del intVar,floatVar  #删除变量
+print intVar;
+
+
+
+
+
+
+# 1. 常用变量
+# intVar=100;
+# floatVar=100.0
+# str="胡明伟"
+#
+# print intVar
+# print floatVar
+# print str
+#
+
+
+# 2. 同时定义多个变量
+# a=b=c=100
+# print a;
+# print b;
+# print c
+#
+# b=199
+# c=100.11
+# print a
+# print b
+# print c
+
