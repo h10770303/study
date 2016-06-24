495de330e2131e89ae0ed2cd46bbd53ea2f18cb5
hu_mingwei <hu_mingwei@cdv.com>
2016-06-24 11:39:38 +0800

    代码更新 最新

--- ThreadPro/src/com/thread/runable/RunAbleTest.java
+++ ThreadPro/src/com/thread/runable/RunAbleTest.java
@@ -1,21 +1,47 @@
 package com.thread.runable;
 
+/**
+ * 使用runable接口实现多线程
+ *   需要注意，启动多线程的时候需要使用Thread的构造方法Tread(Runable target) 创建对象 然后在start进行开启线程
+ * @author hh
+ *
+ */
 public class RunAbleTest implements Runnable {
+	
+	private String name;
 
 	@Override
 	public void run() {
 		System.out.println("调用了线程！！");
 		for (int i = 0; i < 10; i++) {
-			System.out.println("线程i" + i);
+			System.out.println(name+"线程:" + i);
+		}
+		
+		try {
+			// 需要注意的是sleep()是Thread类的方法， 在实现runable接口时，调用sleep需要这样
+			Thread.sleep(100);
+		} catch (Exception e) {
 		}
 	}
 
 	public static void main(String[] args) {
-		RunAbleTest runTest = new RunAbleTest();
-		Thread t1 = new Thread(runTest);
-		Thread t2 = new Thread(runTest);
+		RunAbleTest runTest1=new RunAbleTest("C");
+		RunAbleTest runTest2=new RunAbleTest("D");
+		Thread t1 =new Thread(runTest1);
+		Thread t2 = new Thread(runTest2);
 		t1.start();
 		t2.start();
 	}
+
+	
+	
+	public RunAbleTest() {
+	}
+
+	public RunAbleTest(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
--- memcacheTest/src/com/memcache/test/MemcacheTest.java
+++ memcacheTest/src/com/memcache/test/MemcacheTest.java
@@ -8,25 +8,53 @@ import net.spy.memcached.MemcachedClient;
 
 public class MemcacheTest {
 
+//	public static void main(String[] args) {
+//		 MemCachedClient mcc = new MemCachedClient();
+//		 String[] servers = {"172.27.54.75:11211"};
+//	       Integer[] weights = {3};
+//	       
+//	       // 创建Socket连接池对象
+//	       SockIOPool pool = SockIOPool.getInstance();
+//	       
+//	       // 设置服务器信息
+//	       pool.setServers(servers);
+//	       pool.setWeights(weights);
+//	       pool.setFailover(true);
+//	       
+//	       // 设置初始连接数、最小和最大连接数以及最大处理时间
+//	       pool.setInitConn(5);
+//	       pool.setMinConn(5);
+//	       pool.setMaxConn(250);
+//	       pool.setMaxIdle(1000*60*60*6);
+//	       
+//	       // 设置主线程睡眠时间
+//	       pool.setMaintSleep(30);
+//	       
+//	       // 设置TCP参数、连接超时等
+//	       pool.setNagle(false);
+//	       pool.setSocketTO(3000);
+//	       pool.setSocketConnectTO(0);
+//	       pool.setAliveCheck(true);
+//	       
+//	       // 初始化连接池
+//	       pool.initialize();
+//	       
+//	       // 压缩设置，超过指定大小（单位为K）的数据都会被压缩
+//	       mcc.setCompressEnable(true);
+//	       mcc.setCompressThreshold(64 * 1024);
+//	       
+//	       mcc.add("key1", "122112");
+//	       System.out.println(mcc.get("key1"));
+//	}
 	
+////		//  连接方式2： 使用socket套接字的方式进行连接
 	public static void main(String[] args) throws IOException {
-//		String[] serverList={"172.27.54.75:11211"};
-//		SockIOPool pool=SockIOPool.getInstance();
-//		pool.setServers(serverList);
-//		pool.initialize();
-//		
-//		MemCachedClient mc=new MemCachedClient();
-//		String key="key1";
-//		String value="bbb";
-//		mc.set(key, value);
-		
 		
 		 MemcachedClient client=new MemcachedClient(new InetSocketAddress("172.27.54.75", 11211));
-	      //60是超时数，默认以秒为单位
+//	      //60是超时数，默认以秒为单位
 	      client.set("test", 6000, "1111测试memcache成功了吧");
-	   
-	    
-//	     System.out.println(client.get("test"));
+//	   
+	     System.out.println(client.get("test"));
 	      client.shutdown();//关闭连接
 	}
 }
